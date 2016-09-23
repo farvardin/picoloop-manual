9f5c3ae87e638b7f080d3ab49f9f55db11749a4d
farvardin <eforgeot@gmail.com>
2016-09-23 15:11:01 +0200

    initial commit adding files

---
+++ compile_all.sh
@@ -0,0 +1,22 @@
+
+
+cp includes/commands_pc.t2t includes/default.t2t
+make pdfsmall
+make booklet
+mv picoloop_manual-nup.pdf picoloop_manual_pc.pdf
+mv picoloop_manual_booklet.pdf picoloop_manual_booklet_pc.pdf 
+
+cp includes/commands_psp.t2t includes/default.t2t
+make pdfsmall
+make booklet
+mv picoloop_manual-nup.pdf picoloop_manual_psp.pdf
+mv picoloop_manual_booklet.pdf picoloop_manual_booklet_psp.pdf 
+
+cp includes/commands_pocketchip.t2t includes/default.t2t
+make pdfsmall
+make booklet
+mv picoloop_manual-nup.pdf picoloop_manual_pocketchip.pdf
+mv picoloop_manual_booklet.pdf picoloop_manual_booklet_pocketchip.pdf 
+
+
+cp includes/commands_pc.t2t includes/default.t2t
